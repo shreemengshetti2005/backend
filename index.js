3b80eb4169293292b67e1c99235535e58281fc1d
shreemenghsetti2005 <shreemenghsetti12@gmail.com>
2025-01-12 17:49:41 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config;
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("This is Twitter");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
